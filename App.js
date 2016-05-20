109bd160fb9c1c977367d81937875f7eb943e060
Russ Wells <russ@russdigital.com>
2016-05-19 17:00:32 -0700

    Intro to properties

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render(){
-		return (
-			<div>
-			<h1>Hello World</h1>
-			<b>Bold</b>
-			</div>
-		);
+		let txt = this.props.txt
+		return <h1>{txt}</h1>
 	}
 }
 
-export default App
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this is the default'
+}
+ReactDOM.render(
+	<App cat={5} txt="this is the props value" />,
+	document.getElementById('app')
+);
